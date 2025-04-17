694a661aa170a322758b85848b9d74f267e32a12
krwku <kris.w@ku.th>
2025-04-17 17:42:21 +0700

    Update setup.py
    update v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="course-registration-validator",
-    version="0.1.0",
+    version="0.2.0",
     py_modules=["integrated_solution", "app", "validator", "transcript_editor_app", "batch_processor"],
     packages=find_packages(),
     package_data={
